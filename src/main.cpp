ca5ca274ae7b644e6afee7e8fa4fa0ef6c973235
pedrolopes951 <pedro.miguel.lopes951@gmail.com>
2023-09-11 09:19:02 +0100

    Added Init Window but not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,63 @@
+// Include GLEW
+#include <glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
-#include "GLFW/glfw3.h"
 
 
-
-
-
-int main(int argc, char* argv[])
+GLFWwindow* InitWindow()
 {
-    std::cout << "Testing" << std::endl;
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(960, 540, "Tutorial 02 - Red triangle", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return nullptr;
+
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    //if (glewInit() != GLEW_OK) {
+    //    fprintf(stderr, "Failed to initialize GLEW\n");
+    //    getchar();
+    //    glfwTerminate();
+    //    return nullptr;
+    //}
+
+    //std::cout << "Using GL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    //// Ensure we can capture the escape key being pressed below
+    //glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    //return window;
+}
+
+int main(void)
+{
+    GLFWwindow* window = InitWindow();
+    if (!window)
+        return -1;
+
+
     return 0;
 }
