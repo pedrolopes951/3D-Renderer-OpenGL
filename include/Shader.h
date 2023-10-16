0ed4766075ae660b2cd52196e725c6b53e1bcd0e
pedrolopes951 <pedro.miguel.lopes951@gmail.com>
2023-10-16 08:11:20 +0100

    added shader class methods

--- include/Shader.h
+++ include/Shader.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <string>
 
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+
+};
+
 class Shader
 {
 public:
@@ -12,12 +19,20 @@ public:
     // Utility uniform Functions
     void setBool(const std::string& name, bool value) const;
     void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
-
+    void setFloat2D(const std::string& name,float v0,float v1, float value0, float value1) const;
+    void setFloat3D(const std::string& name, float v0, float v1,float v2, float value0, float value1, float value2) const;
+    
     ~Shader();
 
 private:
+
+    unsigned int CompileShader(unsigned int type, const std::string& source);
+    unsigned int CreateShader(const std::string& vertexSahder, const std::string& fragmentshader);
+    unsigned int GetUniformLocation(const std::string& name);
+
     unsigned int m_id;
+
+    ShaderProgramSource m_sources;
 
 
 };
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,8 +1,15 @@
 #include "Shader.h"
 
+#include "GL/glew.h"
 
-Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath)
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath) : m_sources{vertexShaderPath , fragmentShaderPath }, m_id{0}
 {
+
 }
 
 void Shader::use()
@@ -17,10 +24,30 @@ void Shader::setInt(const std::string& name, int value) const
 {
 }
 
-void Shader::setFloat(const std::string& name, float value) const
+void Shader::setFloat2D(const std::string& name, float v0, float v1, float value0, float value1) const
+{
+}
+
+void Shader::setFloat3D(const std::string& name, float v0, float v1, float v2, float value0, float value1, float value2) const
 {
 }
 
 Shader::~Shader()
 {
+}
+
+unsigned int  Shader::CompileShader(unsigned int type, const std::string& source)
+{
+    return false;
+}
+
+unsigned int Shader::CreateShader(const std::string& vertexSahder, const std::string& fragmentshader)
+{
+
+    return 0;
+}
+
+unsigned int Shader::GetUniformLocation(const std::string& name)
+{
+    return 0;
 }
