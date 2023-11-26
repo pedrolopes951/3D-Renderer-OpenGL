84c691b33244b7d538fdd0686d89fd7f4368a564
pedrolopes951 <pedro.miguel.lopes951@gmail.com>
2023-11-26 15:56:20 +0000

    fixed the bug

--- src/Transformation.cpp
+++ src/Transformation.cpp
@@ -12,15 +12,15 @@ Transformation::~Transformation()
 }
 
 void Transformation::Translate(float x, float y, float z) {
-    m_modelMatrix = glm::translate(m_indentyMatrix, glm::vec3(200.f, 200.f, z));
+    m_modelMatrix = m_projection * glm::translate(m_indentyMatrix, glm::vec3(x, y, z));
 }
 
 void  Transformation::Rotate(float angle, float x, float y, float z) {
-    m_modelMatrix = glm::rotate(m_indentyMatrix, glm::radians(angle), glm::vec3(x, y, z));
+    m_modelMatrix = m_projection * glm::rotate(m_indentyMatrix, glm::radians(angle), glm::vec3(x, y, z));
 }
 
 void  Transformation::Scale(float x, float y, float z) {
-    m_modelMatrix = glm::scale(m_indentyMatrix, glm::vec3(x, y, z));
+    m_modelMatrix = m_projection * glm::scale(m_indentyMatrix, glm::vec3(x, y, z));
 }
 
 void Transformation::Ortho(float left, float right, float bottom, float top)
@@ -32,5 +32,5 @@ void Transformation::Ortho(float left, float right, float bottom, float top)
 
 
 const glm::mat4& Transformation::GetModelMatrix() const {
-    return m_projection*m_modelMatrix;
+    return m_modelMatrix;
 }
